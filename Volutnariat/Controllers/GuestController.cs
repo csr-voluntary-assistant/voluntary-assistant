20cf58657cb2ef9dc508d170f0dc3303c087dba3
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-18 00:43:07 +0300

    - pages for registration as Ong, Doctor, Beneficiary, Volunteer and some other code improvements (#7)

--- Volutnariat/Controllers/GuestController.cs
+++ Volutnariat/Controllers/GuestController.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Volutnariat.Controllers
 {
     public class GuestController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult RegisterAsOng()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterAsDoctor()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterAsBeneficiary()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterAsVolunteer()
         {
             return View();
         }
--- Volutnariat/Controllers/OngsController.cs
+++ Volutnariat/Controllers/OngsController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Volutnariat.Data;
 using Volutnariat.Models;
 
