7a90424b4f7f10f039ed844496610666380afb49
Petrutiu Mihai <petrutiu.mihai@gmail.com>
2020-04-18 21:18:38 +0300

    Add Roles (#12)
    Add email confirmation
    Add SendGrid account in appsettings for easier access for other members
    
    Co-authored-by: Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>

--- Voluntariat/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Voluntariat/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -86,14 +86,14 @@ namespace Voluntariat.Areas.Identity.Pages.Account
                         "/Account/ConfirmEmail",
                         pageHandler: null,
                         values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        protocol: Request.Scheme);                    
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, DisplayConfirmAccountLink= false });
                     }
                     else
                     {
