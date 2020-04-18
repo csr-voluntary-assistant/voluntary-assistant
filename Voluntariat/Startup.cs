3a45b6a698bec96f41a4dcc15bd8825ed6bba141
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-18 14:26:11 +0300

    - seed database only after the migrations have run (#4)

--- Voluntariat/Startup.cs
+++ Voluntariat/Startup.cs
@@ -40,8 +40,6 @@ namespace Voluntariat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<IdentityUser> userManager)
         {
-            ApplicationDbInitializer.SeedUsers(userManager);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -55,6 +53,9 @@ namespace Voluntariat
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            ApplicationDbInitializer.SeedUsers(userManager);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
