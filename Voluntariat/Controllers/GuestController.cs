e8b0b6c620a2bcd4888358d9dadffc360e7cd5b2
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-19 01:06:04 +0300

    - doctors can register

--- Voluntariat/Controllers/GuestController.cs
+++ Voluntariat/Controllers/GuestController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Voluntariat.Framework.Identity;
@@ -11,9 +13,12 @@ namespace Voluntariat.Controllers
     {
         private readonly Data.ApplicationDbContext applicationDbContext;
 
-        public GuestController(Data.ApplicationDbContext applicationDbContext)
+        private readonly UserManager<IdentityUser> userManager;
+
+        public GuestController(Data.ApplicationDbContext applicationDbContext, UserManager<IdentityUser> userManager)
         {
             this.applicationDbContext = applicationDbContext;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
@@ -53,9 +58,46 @@ namespace Voluntariat.Controllers
             return View(ong);
         }
 
+        [HttpGet]
         public IActionResult RegisterAsDoctor()
         {
-            return View();
+            Identity identity = ControllerContext.GetIdentity();
+
+            Doctor doctor = applicationDbContext.Doctors.FirstOrDefault(x => x.Status == DoctorStatus.PendingVerification && x.ID == identity.ID);
+
+            if (doctor != null)
+            {
+                ViewBag.Doctor = doctor;
+
+                return View(null);
+            }
+
+            List<Ong> ongs = applicationDbContext.Ongs.ToList();
+
+            return View(ongs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsDoctor(Guid id)
+        {
+            Identity identity = ControllerContext.GetIdentity();
+
+            Doctor doctor = new Doctor();
+            doctor.ID = identity.ID;
+            doctor.OngID = identity.OngID;
+            doctor.Status = DoctorStatus.PendingVerification;
+
+            IdentityUser identityUser = await userManager.FindByIdAsync(identity.ID.ToString());
+
+            await userManager.RemoveFromRoleAsync(identityUser, Framework.Identity.IdentityRole.Guest);
+
+            await userManager.AddToRoleAsync(identityUser, Framework.Identity.IdentityRole.Doctor);
+
+            applicationDbContext.Doctors.Add(doctor);
+
+            await applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(RegisterAsDoctor));
         }
 
         public IActionResult RegisterAsBeneficiary()
