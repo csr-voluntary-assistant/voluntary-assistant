f3036668ea0b7259899931ece30b0d42d852e12a
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-18 16:45:43 +0300

    - display orders for volunteers

---
+++ Voluntariat/Data/Migrations/20200418133842_Add_Creation_And_Completion_Dates_In_Table_Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Voluntariat.Data.Migrations
+{
+    public partial class Add_Creation_And_Completion_Dates_In_Table_Order : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletionDate",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletionDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Orders");
+        }
+    }
+}
