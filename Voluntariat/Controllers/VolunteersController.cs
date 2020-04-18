55bf5924c47cae2bc6d755ed57c8e06c55c8f863
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-18 15:53:59 +0300

    - Volunteers have their own UI and are redirected directly there (#6)

---
+++ Voluntariat/Controllers/VolunteersController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Voluntariat.Data;
+using Voluntariat.Framework.Identity;
+using Voluntariat.Models;
+
+namespace Voluntariat.Controllers
+{
+    public class VolunteersController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public VolunteersController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            Identity identity = ControllerContext.GetIdentity();
+
+            List<Volunteer> volunteers = await applicationDbContext.Volunteers.Where(x => x.OngID == identity.OngID).ToListAsync();
+
+            foreach (Volunteer volunteer in volunteers)
+            {
+                volunteer.Name = applicationDbContext.Users.Find(volunteer.ID.ToString()).Email;
+            }
+
+            return View(volunteers);
+        }
+
+        // GET: Volunteers/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var volunteer = await applicationDbContext.Volunteers
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+
+            return View(volunteer);
+        }
+
+        // GET: Volunteers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Volunteers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Latitude,Longitude,OngID")] Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                volunteer.ID = Guid.NewGuid();
+                applicationDbContext.Add(volunteer);
+                await applicationDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(volunteer);
+        }
+
+        // GET: Volunteers/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var volunteer = await applicationDbContext.Volunteers.FindAsync(id);
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+            return View(volunteer);
+        }
+
+        // POST: Volunteers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ID,Latitude,Longitude,OngID")] Volunteer volunteer)
+        {
+            if (id != volunteer.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    applicationDbContext.Update(volunteer);
+                    await applicationDbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VolunteerExists(volunteer.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(volunteer);
+        }
+
+        // GET: Volunteers/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var volunteer = await applicationDbContext.Volunteers
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+
+            return View(volunteer);
+        }
+
+        // POST: Volunteers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var volunteer = await applicationDbContext.Volunteers.FindAsync(id);
+            applicationDbContext.Volunteers.Remove(volunteer);
+            await applicationDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VolunteerExists(Guid id)
+        {
+            return applicationDbContext.Volunteers.Any(e => e.ID == id);
+        }
+    }
+}
