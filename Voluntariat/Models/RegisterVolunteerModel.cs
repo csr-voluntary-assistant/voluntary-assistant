4a873985d36228a9834e7f4f22ff2c5a14fd4a8d
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-18 16:22:08 +0300

    - invite a volunteer (#8)

---
+++ Voluntariat/Models/RegisterVolunteerModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Voluntariat.Models
+{
+    public class RegisterVolunteerModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
