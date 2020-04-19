ae53203219d5079031ce6f54d907213ae3a91172
Alexandru Oancea <alx.oancea@gmail.com>
2020-04-19 12:56:33 +0300

    - doctors are redirected to their own page (#15)

---
+++ Voluntariat/Framework/DoctorActionFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Voluntariat.Controllers;
+using Voluntariat.Framework.Identity;
+
+namespace Voluntariat.Framework
+{
+    public class DoctorActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Identity.Identity identity = context.HttpContext.GetIdentity();
+
+            if (identity != null && identity.Role == IdentityRole.Doctor && !(context.Controller is DoctorController controller))
+            {
+                context.Result = new RedirectToActionResult(nameof(DoctorController.Index), nameof(DoctorController)[0..^10], null);
+            }
+        }
+    }
+}
