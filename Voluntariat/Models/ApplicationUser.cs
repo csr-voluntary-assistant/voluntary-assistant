2ed1b04ca5f174539d3a478768377bcba7ff7688
Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>
2020-04-23 20:32:01 +0300

    change default app setting so that it doesn't require confirmed account

--- Voluntariat/Models/ApplicationUser.cs
+++ Voluntariat/Models/ApplicationUser.cs
@@ -8,12 +8,16 @@ namespace Voluntariat.Models
 {
     public class ApplicationUser: IdentityUser
     {
+        [PersonalData]
         public int DialingCode { get; set; }
 
+        [PersonalData]
         public double Latitude { get; set; }
-
+        
+        [PersonalData]
         public double Longitude { get; set; }
 
+        [PersonalData]
         public string Address { get; set; }
     }
 }
