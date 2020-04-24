95bf645cd82aa07ea9b0e8d89c444be9a5ae3154
Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>
2020-04-24 23:05:04 +0300

    Enable 2 FA

--- Voluntariat/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Voluntariat/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -149,7 +149,7 @@ namespace Voluntariat.Areas.Identity.Pages.Account.Manage
         {
             return string.Format(
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Voluntariat"),
+                _urlEncoder.Encode("Volunteero"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
