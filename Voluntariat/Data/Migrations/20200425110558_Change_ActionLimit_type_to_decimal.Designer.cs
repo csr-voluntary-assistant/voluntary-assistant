76b99b9484ff39c746c159eba6bdb87b4418e47a
DiaPasc <dia.pasc@gmail.com>
2020-04-25 15:35:41 +0300

    Registration for Volunteer (#23)
    * Registration for Volunteer
    
    * Code cleanup

--- Voluntariat/Data/Migrations/20200425110558_Change_ActionLimit_type_to_decimal.Designer.cs
+++ Voluntariat/Data/Migrations/20200425110558_Change_ActionLimit_type_to_decimal.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Voluntariat.Data;
 
 namespace Voluntariat.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200425110558_Change_ActionLimit_type_to_decimal")]
+    partial class Change_ActionLimit_type_to_decimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
