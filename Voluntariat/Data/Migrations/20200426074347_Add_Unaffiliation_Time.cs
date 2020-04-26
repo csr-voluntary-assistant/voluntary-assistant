b1f2e5b7b6590a8ab5eb6ddb47649f873135ec15
Florin Pascu <florin.pascu@qubiz.com>
2020-04-26 10:58:45 +0300

    Add migration

---
+++ Voluntariat/Data/Migrations/20200426074347_Add_Unaffiliation_Time.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Voluntariat.Data.Migrations
+{
+    public partial class Add_Unaffiliation_Time : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UnaffiliationStartTime",
+                table: "Volunteers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnaffiliationStartTime",
+                table: "Volunteers");
+        }
+    }
+}
