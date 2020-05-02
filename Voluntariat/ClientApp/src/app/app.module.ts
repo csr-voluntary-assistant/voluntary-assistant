22c8ed9198e67eee5e8403ee38cc0ae8d51c7081
Lidia <lidia_ciurel@yahoo.com>
2020-05-03 00:05:12 +0300

    Nuance changes in code (spacing)

--- Voluntariat/ClientApp/src/app/app.module.ts
+++ Voluntariat/ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
