fa3cc5644761d8f5f0e21de41d405b3df1102b59
Petrutiu Mihai <petrutiu.mihai@gmail.com>
2020-05-03 21:22:27 +0300

    Add swagger with test API controller (#48)
    * Add swagger with test API controller
    
    * Ignore package-lock.json
    
    Co-authored-by: Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>

--- Voluntariat/Startup.cs
+++ Voluntariat/Startup.cs
@@ -9,7 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Voluntariat.Data;
 using Voluntariat.Data.Repositories;
@@ -87,11 +89,21 @@ namespace Voluntariat
             {
                 configuration.RootPath = "wwwroot/dist";
             });
-        }
+            AddSwaggerGen(services);
+        }        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<ApplicationUser> userManager)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -116,6 +128,7 @@ namespace Voluntariat
             app.UseSpaStaticFiles(new StaticFileOptions() { RequestPath = new PathString("/angular") });
 
             app.UseRouting();
+            
 
             app.UseAuthentication();
             app.UseAuthorization();
@@ -127,12 +140,47 @@ namespace Voluntariat
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapRazorPages();
+                
             });
 
             app.UseSpa(spa =>
             {
                 //spa.Options.DefaultPage = new PathString("/angular");
                 spa.Options.SourcePath = "wwwroot";
+            });
+        }
+
+        private static void AddSwaggerGen(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "3.0" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },new List<string>()
+                    }
+                });
             });
         }
 
