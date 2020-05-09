9df62a3ac4c251456b7692e479b2ac5078d06987
DiaPasc <dia.pasc@gmail.com>
2020-05-10 00:41:38 +0300

    Details page improvements (#75)

--- src/Voluntariat/ClientApp/src/app/shared/angular-material.module.ts
+++ src/Voluntariat/ClientApp/src/app/shared/angular-material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
@@ -13,7 +13,8 @@ import { MatTableModule } from '@angular/material/table';
   ],
   exports: [
     MatSliderModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule
   ]
 })
 
