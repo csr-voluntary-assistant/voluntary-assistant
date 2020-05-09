d0ea326ce690bf13715187ad560111a0914ef73f
dia.pasc <dia.pasc@gmail.com>
2020-05-09 23:18:30 +0300

    Include AngularMaterialModule in AdminNgosModule

--- src/Voluntariat/ClientApp/src/app/admin/admin-ngos/admin-ngos.module.ts
+++ src/Voluntariat/ClientApp/src/app/admin/admin-ngos/admin-ngos.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AdminNgosListComponent } from './admin-ngos-list/admin-ngos-list.component';
 import { AdminNgosVerifyComponent } from './admin-ngos-verify/admin-ngos-verify.component';
 import { RouterModule } from '@angular/router';
+import { AngularMaterialModule } from '../../shared/angular-material.module';
 
 @NgModule({
   declarations: [AdminNgosListComponent, AdminNgosVerifyComponent],
   imports: [
     CommonModule,
+    AngularMaterialModule,
     RouterModule.forRoot([
       { path: 'admin-ngos-list', component: AdminNgosListComponent },
       { path: 'admin-ngos-verify/:id', component: AdminNgosVerifyComponent },
