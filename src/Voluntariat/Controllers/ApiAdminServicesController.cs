e8e5f4e06d13e21ec65bc009bab8deaf654dfcf7
Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>
2020-05-11 22:42:35 +0300

    Add Extra services

---
+++ src/Voluntariat/Controllers/ApiAdminServicesController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Voluntariat.Data;
+using Voluntariat.Framework.Identity;
+using Voluntariat.Models;
+
+namespace Voluntariat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = CustomIdentityRole.Admin)]
+    public class ApiAdminServicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApiAdminServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiAdminServices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Service>>> GetServices()
+        {
+            return await _context.Services.ToListAsync();
+        }
+
+        // GET: api/ApiAdminServices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Service>> GetService(Guid id)
+        {
+            var service = await _context.Services.FindAsync(id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return service;
+        }
+
+        // PUT: api/ApiAdminServices/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutService(Guid id, Service service)
+        {
+            if (id != service.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(service).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServiceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiAdminServices
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Service>> PostService(Service service)
+        {
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetService", new { id = service.ID }, service);
+        }
+
+        // DELETE: api/ApiAdminServices/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Service>> DeleteService(Guid id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            _context.Services.Remove(service);
+            await _context.SaveChangesAsync();
+
+            return service;
+        }
+
+        private bool ServiceExists(Guid id)
+        {
+            return _context.Services.Any(e => e.ID == id);
+        }
+    }
+}
