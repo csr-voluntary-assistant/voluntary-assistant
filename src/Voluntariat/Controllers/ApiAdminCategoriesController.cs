978179df2afec57abec9df8b7650d4023715cd16
Petrutiu Mihai <petrutiu.mihai@gmail.com>
2020-05-11 22:46:16 +0300

    Add Extra services (#88)
    Co-authored-by: Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>

---
+++ src/Voluntariat/Controllers/ApiAdminCategoriesController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Voluntariat.Data;
+using Voluntariat.Framework.Identity;
+using Voluntariat.Models;
+
+namespace Voluntariat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = CustomIdentityRole.Admin)]
+    public class ApiAdminCategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApiAdminCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiAdminCategories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        // GET: api/ApiAdminCategories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/ApiAdminCategories/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(Guid id, Category category)
+        {
+            if (id != category.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiAdminCategories
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategory", new { id = category.ID }, category);
+        }
+
+        // DELETE: api/ApiAdminCategories/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Category>> DeleteCategory(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _context.Categories.Any(e => e.ID == id);
+        }
+    }
+}
