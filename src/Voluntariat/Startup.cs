5c4703d3c4b1394b79d3211441a274cd9fb76094
Alexandru Oancea <alx.oancea@gmail.com>
2020-05-15 11:31:24 +0300

    test (#90)

--- src/Voluntariat/Startup.cs
+++ src/Voluntariat/Startup.cs
@@ -106,6 +106,8 @@ namespace Voluntariat
             }
         }
 
+        // test commit 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<ApplicationUser> userManager)
         {
